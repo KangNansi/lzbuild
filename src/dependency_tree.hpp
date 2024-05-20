b082e2db1c7b11c2c55f33db53e2c8f6f72a97c2
KangNansi <evillz@hotmail.com>
2024-05-20 23:33:18 +0800

    fix dependency check

--- src/dependency_tree.hpp
+++ src/dependency_tree.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <unordered_set>
 #include <optional>
+#include <iostream>
 
 class dependency_tree
 {
@@ -12,7 +13,8 @@ class dependency_tree
 public:
     void add(std::filesystem::path file, const std::vector<std::filesystem::path>& include_folders);
     bool need_rebuild(std::filesystem::path source, std::filesystem::file_time_type timestamp);
-
+    void print(std::ostream& output);
+ 
 private:
     bool need_rebuild(std::filesystem::path source, std::filesystem::file_time_type timestamp, std::unordered_set<std::string>& ignore);
 };
